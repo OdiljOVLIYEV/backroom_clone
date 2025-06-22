d10c69710d3c046777b85cea8c9e9e64291b1872
OdiljOVLIYEV <109040431+OdiljOVLIYEV@users.noreply.github.com>
2025-06-22 10:36:08 +0500

    231

---
+++ Assets/Scripts/PlayerListItemUI.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerListItemUI : MonoBehaviour
+{
+	public TMP_Text nameText;
+	public TMP_Text roleText;
+	public TMP_Text statusText;
+	public Button killButton;
+}
--- Assets/Scripts/PlayerListUI.cs
+++ Assets/Scripts/PlayerListUI.cs
@@ -7,7 +7,7 @@ using ExitGames.Client.Photon;
 
 public class PlayerListUI : MonoBehaviourPunCallbacks
 {
-	public GameObject playerItemPrefab; // Prefab ichida TMP_Text va Kick Button
+	public GameObject playerItemPrefab; // Prefab ichida PlayerListItemUI component
 	public Transform listParent; // UI Panel ichidagi Content
 
 	private void Start()
@@ -16,7 +16,6 @@ public class PlayerListUI : MonoBehaviourPunCallbacks
 		RefreshPlayerList();
 	}
 
-	// Barcha o'yinchilar ro'yxatini UIga chiqaradi
 	public void RefreshPlayerList()
 	{
 		foreach (Transform child in listParent)
@@ -30,58 +29,49 @@ public class PlayerListUI : MonoBehaviourPunCallbacks
 		{
 			GameObject item = Instantiate(playerItemPrefab, listParent);
 
-			TMP_Text[] texts = item.GetComponentsInChildren<TMP_Text>();
-			TMP_Text nameText = texts[0];
-			TMP_Text roleText = texts[1];
-			TMP_Text statusText = texts[2];
+			PlayerListItemUI ui = item.GetComponent<PlayerListItemUI>();
 
-			Button killButton = item.GetComponentInChildren<Button>(); // KillButton
+			if (ui == null)
+			{
+				Debug.LogError("⚠️ PlayerListItemUI component topilmadi! Prefabga qo‘shishni unutmang.");
+				Destroy(item);
+				continue;
+			}
 
-			nameText.text = player.NickName;
-			roleText.text = "NOMALUM";
-			//statusText.text = "HOLAT: ?";
+			ui.nameText.text = player.NickName;
+			ui.roleText.text = "NOMALUM";
+			//ui.statusText.text = "HOLAT: ?";
 
 			foreach (PlayerRole pr in allRoles)
 			{
 				if (pr.photonView.Owner == player)
 				{
-					// Faqat o‘z rolini ko‘rsatadi
+					// O'z rolini ko'rsatadi
 					if (player == PhotonNetwork.LocalPlayer)
-					{
-						roleText.text = pr.role.ToUpper();
-					}
+						ui.roleText.text = pr.role.ToUpper();
 					else
-					{
-						roleText.text = "NOMALUM";
-					}
+						ui.roleText.text = "NOMALUM";
 
-					statusText.text = pr.isAlive ? "🟢 TIRIK" : "☠️ O‘LIK";
+					ui.statusText.text = pr.isAlive ? "🟢 TIRIK" : "☠️ O‘LIK";
 
 					if (!pr.isAlive)
 					{
-						nameText.color = Color.gray;
-						roleText.color = Color.gray;
-						statusText.color = Color.gray;
+						ui.nameText.color = Color.gray;
+						ui.roleText.color = Color.gray;
+						ui.statusText.color = Color.gray;
 					}
 
-					// 👉 KILL BUTTON LOGIC
-					// Faqat mafia va tirik bo‘lsa, boshqalarga tugmani ko‘rsat
 					PlayerRole myRole = GetMyPlayerRole();
+
 					if (myRole != null && myRole.role == "Mafia" && myRole.isAlive && pr.isAlive && player != PhotonNetwork.LocalPlayer)
 					{
-						killButton.gameObject.SetActive(true);
-						Debug.Log("SIZ MAFIASIZ");
-
-						// Targetga Kill RPC yuborish
-						killButton.onClick.AddListener(() =>  
-						{  
-							pr.Kill(); // bu endi RPC orqali barcha clientlarga yuboriladi  
-						});
+						ui.killButton.gameObject.SetActive(true);
+						ui.killButton.onClick.RemoveAllListeners();
+						ui.killButton.onClick.AddListener(() => pr.Kill());
 					}
 					else
 					{
-						Debug.Log("SIZ MAFIASIZ EMASSIZ");
-						killButton.gameObject.SetActive(false);
+						ui.killButton.gameObject.SetActive(false);
 					}
 
 					break;
@@ -90,7 +80,6 @@ public class PlayerListUI : MonoBehaviourPunCallbacks
 		}
 	}
 
-	// O‘z rolini olish uchun yordamchi funksiya
 	private PlayerRole GetMyPlayerRole()
 	{
 		PlayerRole[] roles = FindObjectsOfType<PlayerRole>();
@@ -107,33 +96,6 @@ public class PlayerListUI : MonoBehaviourPunCallbacks
 		RefreshPlayerList();
 	}
 
-
-
-
-	// Kick funksiyasi faqat masterclientda ishlaydi
-	/*void KickPlayer(int actorNumber)
-	{
-		if (PhotonNetwork.IsMasterClient)
-		{
-			Player playerToKick = null;
-			foreach (var player in PhotonNetwork.PlayerList)
-			{
-				if (player.ActorNumber == actorNumber)
-				{
-					playerToKick = player;
-					break;
-				}
-			}
-
-			if (playerToKick != null)
-			{
-				PhotonNetwork.CloseConnection(playerToKick);
-				Debug.Log($"Player {playerToKick.NickName} kicked.");
-			}
-		}
-	}*/
-
-	// Har doim yangi player kelsa yoki ketsa ro'yxatni yangilaymiz
 	public override void OnPlayerEnteredRoom(Player newPlayer)
 	{
 		RefreshPlayerList();
